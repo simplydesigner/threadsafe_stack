ad3c5c5b978ea14851870499128f84e2cfb68dd4
simplydesigner <simplydesigner@ya.ru>
2016-08-19 11:46:31 +0300

    Initial Commit

---
+++ threadsafe_stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  threadsafe_stack
+//
+//  Created by simplydesigner on 8/19/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
